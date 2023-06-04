fd719dd6a556e6aa50888f15cc75e537e2404d62
andres <analisis@karisma.org.co>
2023-06-04 14:35:42 -0500

    chages to seach.py to choose data directory

--- search.py
+++ search.py
@@ -5,10 +5,13 @@ import os
 import re
 import sys
 import subprocess
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QLineEdit, QTableWidget, QTableWidgetItem, QTextEdit, QSplitter, QDialog, QLabel, QPlainTextDocumentLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QLineEdit, QTableWidget, QTableWidgetItem, QTextEdit, QSplitter, QDialog, QLabel, QPlainTextDocumentLayout, QFileDialog
 from PyQt5.QtCore import Qt, QProcess
 from PyQt5.QtGui import QTextCursor, QTextCharFormat, QTextDocument, QColor
 
+SUMMARIZED_DIR = None
+SUMMARIZED_NUM_FILES = None
+SEARCHES_DIR = None
 
 def spanish_friendly(text):
     vowels = ['a', 'e', 'i', 'o', 'u']
@@ -20,6 +23,7 @@ def spanish_friendly(text):
         else:
             spanish_friendly_str += letter
     return spanish_friendly_str
+
 
 class SearchApp(QMainWindow):
     def __init__(self):
@@ -47,6 +51,11 @@ class SearchApp(QMainWindow):
         container.setLayout(layout)
         self.setCentralWidget(container)
 
+        # Get data dir
+        self.data_dir = str(QFileDialog.getExistingDirectory(self, "Select Data Directory"))
+        init_dialog = InitDialog(self.data_dir)
+        init_dialog.exec_()
+
         self.populate_search_history()
 
     def run_search_command(self):
@@ -60,13 +69,14 @@ class SearchApp(QMainWindow):
 
         # Replace spaces in the search term with underscores for the output file name
         output_file = search_term.replace(" ", "_") + ".txt"
+        output_file_fullpath = os.path.join(SEARCHES_DIR, output_file)
 
         # Make search_term spanish accent spanish_friendly
         spanish_friendly_str = spanish_friendly(search_term)
 
         # Construct the search command
         # command = f'time find summarized/ -type f -print0 | pv -0 -s 3330608 | xargs -0 -P 5 grep -R -i -m 1 "{search_term}" > searches/{output_file}'
-        command = f'find summarized/ -type f -print0 | pv -0 -n -s 7151 | xargs -0 -P 1 grep -R -i -m 1 "{spanish_friendly_str}" > test_search.txt'
+        command = f'find {SUMMARIZED_DIR} -type f -print0 | pv -0 -n -s {SUMMARIZED_NUM_FILES} | xargs -0 -P 5 grep -R -i -m 1 "{spanish_friendly_str}" > {output_file_fullpath}'
 
         process = QProcess()
         process.readyReadStandardError.connect(lambda: self.update_search_status(process))
@@ -76,7 +86,7 @@ class SearchApp(QMainWindow):
         self.populate_search_history()
 
     def update_search_status(self, process):
-        # Read the output from the process
+        # Read the output from the processSEARCHES_DIR
         output = process.readAllStandardError().data().decode()
         output = output.strip()
         try:
@@ -88,8 +98,7 @@ class SearchApp(QMainWindow):
     def search_finished(self):
         self.search_button.setEnabled (True)
         self.search_button.setText ('Search')
-
-
+        self.populate_search_history()
 
     def populate_search_history(self):
         # Clear the search history table
@@ -97,13 +106,14 @@ class SearchApp(QMainWindow):
         self.search_history_table.setRowCount(0)
 
         # Get the list of search history files
-        search_files = [f for f in os.listdir("searches/") if f.endswith(".txt")]
+        search_files = [f for f in os.listdir(SEARCHES_DIR) if f.endswith(".txt")]
 
         # Populate the search history table
         for i, file_name in enumerate(search_files):
             search_term = os.path.splitext(file_name)[0].replace("_", " ")
-            results = sum(1 for _ in open(f"searches/{file_name}", encoding="latin-1"))
-            create_date = datetime.datetime.fromtimestamp(os.path.getctime(f"searches/{file_name}")).strftime('%c')
+            result_file = os.path.join(SEARCHES_DIR, file_name)
+            results = sum(1 for _ in open(f"{result_file}", encoding="latin-1"))
+            create_date = datetime.datetime.fromtimestamp(os.path.getctime(f"{result_file}")).strftime('%c')
             self.search_history_table.insertRow(i)
             self.search_history_table.setItem(i, 0, QTableWidgetItem(search_term))
             self.search_history_table.setItem(i, 1, QTableWidgetItem(str(results)))
@@ -111,7 +121,7 @@ class SearchApp(QMainWindow):
 
     def show_search_result(self, row, _):
         file_name = self.search_history_table.item(row, 0).text().replace(" ", "_") + ".txt"
-        file_path = f"searches/{file_name}"
+        file_path = os.path.join(SEARCHES_DIR, file_name)
         result_dialog = ResultDialog(file_path)
         result_dialog.exec_()
 
@@ -178,6 +188,40 @@ class ResultDialog(QDialog):
         self.search_result_text.setDocument(highlighter)
         self.search_result_text.setTextCursor(cursor)
         self.search_result_text.ensureCursorVisible()
+
+
+class InitDialog(QDialog):
+    def __init__(self, data_dir):
+        super().__init__()
+
+        global SUMMARIZED_DIR
+        global SUMMARIZED_NUM_FILES
+        global SEARCHES_DIR
+
+        self.setWindowTitle("Configuring...")
+
+        self.message_label = QLabel("Configuring the system... Don't close this window")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.message_label)
+
+        self.setLayout(layout)
+
+        # check if summarized and searches are on the data_dir
+        dirs_in_data_dir =  os.listdir(data_dir)
+        if 'summarized' in dirs_in_data_dir:
+            SUMMARIZED_DIR = os.path.join(data_dir, 'summarized')
+            SUMMARIZED_NUM_FILES = sum([len(files) for r, d, files in os.walk("summarized")])
+        else:
+            self.message_label.setText("ERROR: 'summarized' folder not found")
+            return None
+
+        # setup SEARCHES_DIR
+        SEARCHES_DIR = os.path.join(data_dir, 'searches')
+        if 'searches' not in dirs_in_data_dir:
+            os.mkdir(SEARCHES_DIR)
+
+        self.message_label.setText("READY: all good, close this window now.")
 
 
 if __name__ == "__main__":
